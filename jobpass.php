e401ea64dd68a79b6b40e9ae1905ae222d646e5d
Paul Lefizelier <paul@jobpass.com>
2022-07-14 13:46:23 +0200

    feat(front): add job search functionnality

--- jobpass.php
+++ jobpass.php
@@ -148,3 +148,15 @@ function joboffers_archive( $template ) {
   }
   return $template;
 }
+
+function template_chooser($template)   
+{    
+  global $wp_query;   
+  $post_type = get_query_var('post_type');   
+  if( $wp_query->is_search && $post_type == 'joboffers' )   
+  {
+    return locate_template(JOBPASS_PATH . '/public/job-search.php');  //  redirect to archive-search.php
+  }   
+  return $template;   
+}
+add_filter('template_include', 'template_chooser');    
